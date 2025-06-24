98ebf8f1673245bbefe96929f308a324ff0eb64c
Galaxy <13087482+GalaxyPw@user.noreply.gitee.com>
2025-06-24 18:32:09 +0800

    Init project

---
+++ src/main/java/org/example/xiaomibms/XiaomiBmsApplication.java
@@ -0,0 +1,13 @@
+package org.example.xiaomibms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaomiBmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaomiBmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/xiaomibms/XiaomiBmsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.xiaomibms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaomiBmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
