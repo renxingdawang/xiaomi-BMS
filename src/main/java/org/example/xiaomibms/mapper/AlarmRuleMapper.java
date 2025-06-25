65a9c1417f0f37609a0d247d72823abc83a08139
Galaxy <13087482+GalaxyPw@user.noreply.gitee.com>
2025-06-25 11:55:10 +0800

    Add AlarmRuleMapper

--- src/main/java/org/example/xiaomibms/mapper/AlarmRuleMapper.java
+++ src/main/java/org/example/xiaomibms/mapper/AlarmRuleMapper.java
@@ -1,4 +1,15 @@
 package org.example.xiaomibms.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.example.xiaomibms.entity.AlarmRule;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Mapper
 public interface AlarmRuleMapper {
+    List<AlarmRule> findRules(@Param("batteryType") String batteryType,
+                              @Param("diffType") String diffType,
+                              @Param("diffValue") BigDecimal diffValue);
 }
