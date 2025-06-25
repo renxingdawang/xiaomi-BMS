0326e69d10b984f387cc50d262ad1eec43a9ca04
Galaxy <13087482+GalaxyPw@user.noreply.gitee.com>
2025-06-25 11:54:47 +0800

    Add AlarmInfoMapper

--- src/main/java/org/example/xiaomibms/mapper/AlarmInfoMapper.java
+++ src/main/java/org/example/xiaomibms/mapper/AlarmInfoMapper.java
@@ -1,10 +1,13 @@
 package org.example.xiaomibms.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.example.xiaomibms.entity.AlarmInfo;
 import org.example.xiaomibms.entity.VehicleInfo;
 
 import java.util.List;
+@Mapper
 public interface AlarmInfoMapper {
-
+    int insert(AlarmInfo alarmInfo);
+    List<AlarmInfo> selectByVid(String vid);
 }
 
